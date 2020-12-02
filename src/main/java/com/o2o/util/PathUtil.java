7c2d938ef73e9b680dacb3b6899efd7ee1a9a48e
Steve <Stevedc23@users.noreply.github.com>
2020-12-01 20:57:15 -0500

    Create PathUtil.java

---
+++ src/main/java/com/o2o/util/PathUtil.java
@@ -0,0 +1,23 @@
+package com.o2o.util;
+
+public class PathUtil {
+
+    private static String separator = System.getProperty("file.separator");
+
+    public static String getImgBasePath() {
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if(os.toLowerCase().startsWith("win")) {
+            basePath = "F:/java/image/";
+        } else {
+            basePath = "/home/image";
+        }
+        basePath = basePath.replace("/", separator);
+        return basePath;
+    }
+
+    public static String getStoreImagePath(long storeId) {
+        String imagePath = "/upload/item/store" + storeId + "/";
+        return imagePath.replace("/", separator);
+    }
+}

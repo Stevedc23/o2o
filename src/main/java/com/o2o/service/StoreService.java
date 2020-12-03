9d8947602794cf0437a24b28d798666bbe636d10
Steve <Stevedc23@users.noreply.github.com>
2020-12-03 17:05:49 -0500

    Update StoreService.java

--- src/main/java/com/o2o/service/StoreService.java
+++ src/main/java/com/o2o/service/StoreService.java
@@ -2,10 +2,12 @@ package com.o2o.service;
 
 import com.o2o.dto.StoreExecution;
 import com.o2o.entity.Store;
+import com.o2o.exceptions.StoreOperationException;
 
 import java.io.File;
+import java.io.InputStream;
 
 public interface StoreService {
 
-    StoreExecution addStore(Store store, File storeImg);
+    StoreExecution addStore(Store store, InputStream storeImgInputStream, String fileName) throws StoreOperationException;
 }

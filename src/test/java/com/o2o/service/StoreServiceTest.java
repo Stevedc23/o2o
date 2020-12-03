1b179ade239a4b87574add400978e189ec72e2be
Steve <Stevedc23@users.noreply.github.com>
2020-12-03 17:06:07 -0500

    Update StoreServiceTest.java

--- src/test/java/com/o2o/service/StoreServiceTest.java
+++ src/test/java/com/o2o/service/StoreServiceTest.java
@@ -7,10 +7,14 @@ import com.o2o.entity.Store;
 import com.o2o.entity.StoreCategory;
 import com.o2o.entity.UserInfo;
 import com.o2o.enums.StoreStateEnum;
+import com.o2o.exceptions.StoreOperationException;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -21,7 +25,7 @@ public class StoreServiceTest extends BaseTest {
     private StoreService storeService;
 
     @Test
-    public void testAddStore(){
+    public void testAddStore() throws StoreOperationException, FileNotFoundException {
         Store store = new Store();
         UserInfo owner = new UserInfo();
         Area area = new Area();
@@ -34,10 +38,10 @@ public class StoreServiceTest extends BaseTest {
         store.setOwner(owner);
         store.setArea(area);
         store.setStoreCategory(storeCategory);
-        store.setStoreName("Test Store1");
-        store.setStoreDesc("Test1");
-        store.setStoreAddr("test1");
-        store.setPhone("test1");
+        store.setStoreName("Test Store3");
+        store.setStoreDesc("Test3");
+        store.setStoreAddr("test3");
+        store.setPhone("test3");
 
         store.setCreateTime(new Date());
         store.setEnableStatus(StoreStateEnum.CHECK.getState());
@@ -45,7 +49,9 @@ public class StoreServiceTest extends BaseTest {
 
         File storeImg = new File("F:\\Java\\tree.jpg");
 
-        StoreExecution se = storeService.addStore(store, storeImg);
+        InputStream inputStream = new FileInputStream(storeImg);
+
+        StoreExecution se = storeService.addStore(store, inputStream, storeImg.getName());
         assertEquals(StoreStateEnum.CHECK.getState(), se.getState());
     }
 }

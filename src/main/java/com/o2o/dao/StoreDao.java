0911fc40fdfefedc900f1133eb491344b6894f2d
Steve <Stevedc23@users.noreply.github.com>
2020-12-14 16:56:18 -0500

    Update StoreDao.java

--- src/main/java/com/o2o/dao/StoreDao.java
+++ src/main/java/com/o2o/dao/StoreDao.java
@@ -5,6 +5,13 @@ import com.o2o.entity.Store;
 public interface StoreDao {
 
     /**
+     * search store via store id
+     * @param storeId
+     * @return store
+     */
+    Store queryByStoreId(long storeId);
+
+    /**
      * Add store
      * @param store
      * @return
